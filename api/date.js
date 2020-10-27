838bc8a54e1be004acadcd3ec106384c97c5add8
RaviRaj <raviraj636@gmail.com>
2020-10-28 01:00:14 +0530

    Updates

--- api/date.js
+++ api/date.js
@@ -1,4 +1,25 @@
+
+
 module.exports = (req, res) => {
+
+const sqlite3 = require('sqlite3').verbose();
+
+// open database in memory
+let db = new sqlite3.Database(':memory:', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+});
+
+// close the database connection
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
+});
+
   const date = new Date().toString();
   res.status(200).send(date);
 };
